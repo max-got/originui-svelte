f0be812ba08c08e49026d34082bf4bdd5062680e
saya <sametcetinkaya00s@gmail.com>
2024-11-26 06:07:14 +0300

    add select component

---
+++ src/lib/components/ui/select/select-group-heading.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { Select as SelectPrimitive } from 'bits-ui';
+	import { cn } from '$lib/utils.js';
+
+	let {
+		ref = $bindable(null),
+		class: className,
+		...restProps
+	}: SelectPrimitive.GroupHeadingProps = $props();
+</script>
+
+<SelectPrimitive.GroupHeading
+	bind:ref
+	class={cn('py-1.5 pe-2 ps-8 text-xs font-medium text-muted-foreground', className)}
+	{...restProps}
+/>
