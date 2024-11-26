fc9d1da9ed6292deb4b759f6a6733e6c2ec9e96b
saya <sametcetinkaya00s@gmail.com>
2024-11-26 09:41:02 +0300

    fix select-native bug caused by multiple prop

--- src/lib/components/ui/select-native.svelte
+++ src/lib/components/ui/select-native.svelte
@@ -11,8 +11,13 @@
 		ref = $bindable(null),
 		value = $bindable(),
 		children,
+		multiple,
 		...restProps
 	}: WithElementRef<HTMLSelectAttributes> = $props();
+
+	$effect(() => {
+		if (multiple) ref?.setAttribute('multiple', 'true');
+	});
 </script>
 
 <div class="relative">
@@ -21,16 +26,14 @@
 		bind:value
 		class={cn(
 			'peer inline-flex w-full cursor-pointer appearance-none items-center rounded-lg border border-input bg-background text-sm text-foreground shadow-sm shadow-black/5 transition-shadow focus-visible:border-ring focus-visible:outline-none focus-visible:ring-[3px] focus-visible:ring-ring/20 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 has-[option[disabled]:checked]:text-muted-foreground',
-			restProps.multiple
-				? 'py-1 [&>*]:px-3 [&>*]:py-1 [&_option:checked]:bg-accent'
-				: 'h-9 pe-8 ps-3',
+			multiple ? 'py-1 [&>*]:px-3 [&>*]:py-1 [&_option:checked]:bg-accent' : 'h-9 pe-8 ps-3',
 			className
 		)}
 		{...restProps}
 	>
 		{@render children?.()}
 	</select>
-	{#if !restProps.multiple}
+	{#if !multiple}
 		<span
 			class="pointer-events-none absolute inset-y-0 end-0 flex h-full w-9 items-center justify-center text-muted-foreground/80 peer-disabled:opacity-50"
 		>
