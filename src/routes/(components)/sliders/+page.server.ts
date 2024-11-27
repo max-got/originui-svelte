e532d02779ac986141f77e686acad46e02952cdb
saya <sametcetinkaya00s@gmail.com>
2024-11-27 04:04:15 +0300

    add sliders route

---
+++ src/routes/(components)/sliders/+page.server.ts
@@ -0,0 +1,9 @@
+import type { PageServerLoad } from './$types.js';
+
+import { fetchComponentsFromAPI } from '$lib/utils/handleComponentSource.js';
+
+export const load = (async ({ fetch }) => {
+	return {
+		componentMetadata: await fetchComponentsFromAPI(fetch, 'sliders.json')
+	};
+}) satisfies PageServerLoad;
