9b1e7a62cbe0b38ae83747835ba603d3b7f0f565
saya <sametcetinkaya00s@gmail.com>
2024-12-10 19:13:00 +0300

    dialog-01

---
+++ src/lib/components/dialogs/dialog-01.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { buttonVariants } from '../ui/button.svelte';
+	import * as AlertDialog from '$lib/components/ui/alert-dialog/index.js';
+</script>
+
+<AlertDialog.Root>
+	<AlertDialog.Trigger class={buttonVariants({ variant: 'outline' })}>
+		Alert dialog
+	</AlertDialog.Trigger>
+	<AlertDialog.Content>
+		<AlertDialog.Header>
+			<AlertDialog.Title>Are you sure?</AlertDialog.Title>
+			<AlertDialog.Description>
+				Take a moment to review the details provided to ensure you understand the implications.
+			</AlertDialog.Description>
+		</AlertDialog.Header>
+		<AlertDialog.Footer>
+			<AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+			<AlertDialog.Action>Okay</AlertDialog.Action>
+		</AlertDialog.Footer>
+	</AlertDialog.Content>
+</AlertDialog.Root>
