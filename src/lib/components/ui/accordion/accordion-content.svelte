6194550839bf3f02d07613c16cb99546f1073979
saya <sametcetinkaya00s@gmail.com>
2024-12-11 08:04:36 +0300

    add accordion component

---
+++ src/lib/components/ui/accordion/accordion-content.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { Accordion as AccordionPrimitive, type WithoutChild } from 'bits-ui';
+	import { cn } from '$lib/utils.js';
+
+	let {
+		ref = $bindable(null),
+		class: className,
+		children,
+		...restProps
+	}: WithoutChild<AccordionPrimitive.ContentProps> = $props();
+</script>
+
+<AccordionPrimitive.Content
+	bind:ref
+	class="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-sm transition-all"
+	{...restProps}
+>
+	<div class={cn('pb-4 pt-0', className)}>
+		{@render children?.()}
+	</div>
+</AccordionPrimitive.Content>
---
+++ src/lib/components/ui/accordion/accordion-trigger.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { Accordion as AccordionPrimitive, type WithoutChild } from 'bits-ui';
+	import ChevronDown from 'lucide-svelte/icons/chevron-down';
+	import { cn } from '$lib/utils.js';
+
+	let {
+		ref = $bindable(null),
+		class: className,
+		level = 3,
+		children,
+		...restProps
+	}: WithoutChild<AccordionPrimitive.TriggerProps> & {
+		level?: AccordionPrimitive.HeaderProps['level'];
+	} = $props();
+</script>
+
+<AccordionPrimitive.Header {level} class="flex">
+	<AccordionPrimitive.Trigger
+		bind:ref
+		class={cn(
+			'flex flex-1 items-center justify-between py-4 text-left font-semibold transition-all hover:underline [&[data-state=open]>svg]:rotate-180',
+			className
+		)}
+		{...restProps}
+	>
+		{@render children?.()}
+		<ChevronDown
+			size={16}
+			stroke-width={2}
+			aria-hidden
+			class="shrink-0 opacity-60 transition-transform duration-200"
+		/>
+	</AccordionPrimitive.Trigger>
+</AccordionPrimitive.Header>
