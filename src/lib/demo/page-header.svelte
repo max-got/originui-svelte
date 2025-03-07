e9640739b1ab75fc737460721ae512875619246d
Max G. <max.gottschalk@mailbox.org>
2025-03-07 19:03:46 +0100

    refactor: remove unused Svelte type import from page header

--- src/lib/demo/page-header.svelte
+++ src/lib/demo/page-header.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import type { WithElementRef } from 'bits-ui';
-	import type { Snippet } from 'svelte';
 	import type { HTMLAttributes } from 'svelte/elements';
 
 	import { cn } from '$lib/utils';
