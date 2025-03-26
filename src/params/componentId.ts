a4d24d55630539c1eaf96b38589f3f6e5567faba
Max G. <max.gottschalk@mailbox.org>
2025-03-26 19:30:40 +0100

    feat: refactor component registry and metadata management with auto-generated directories and states

--- src/params/componentId.ts
+++ src/params/componentId.ts
@@ -1,13 +1,14 @@
 import type { ParamMatcher } from '@sveltejs/kit';
+import type { OUIReadyComponents } from '$lib/componentRegistry.types';
 
-import { OUI_DIRECTORIES } from '$lib/componentRegistry.types';
+import { OUI_DIRECTORIES } from '$lib/componentRegistry.components';
 
-export const match = ((
-	param: string
-): param is (typeof OUI_DIRECTORIES)[keyof typeof OUI_DIRECTORIES]['components'][number] => {
+export const match = ((param: string): param is OUIReadyComponents => {
+	console.log('param', param);
 	return Object.values(OUI_DIRECTORIES).some((route) =>
 		route.components.some((componentId) => {
 			const componentName = componentId.split('.');
+			console.log('componentName', componentName);
 			return (
 				(!componentId.includes('.todo.') || componentId.includes('.soon.')) &&
 				componentName[0] === param
---
+++ src/routes/+layout.server.ts
@@ -0,0 +1,43 @@
+import { getComponentRegistry } from '$lib/componentRegistry';
+import type { LayoutServerLoad } from './$types';
+
+export const load = (async () => {
+	const instance = await getComponentRegistry();
+	const files = instance.getRegistryMetaData();
+
+	const entries = Object.entries(files.directoriesBreakdown);
+	const links = entries.map(([directory, metadata]) => ({
+		llms: {
+			label: directory + '.txt',
+			href: metadata.llmsTextUrl,
+			ariaLabel: 'LLMs.txt for ' + directory
+		},
+		components: {
+			label: directory + '.json',
+			href: metadata.apiUrl,
+			ariaLabel: 'Raw JSON for ' + directory
+		}
+	}));
+
+	const footerLinks = [
+		{
+			title: 'llms.txt',
+			links: [
+				{
+					label: 'llms.txt',
+					href: '/llms.txt',
+					ariaLabel: 'LLMs.txt for all components'
+				},
+				...links.map((link) => link.llms)
+			]
+		},
+		{
+			title: 'components.json',
+			links: links.map((link) => link.components)
+		}
+	];
+
+	return {
+		footerLinks
+	};
+}) satisfies LayoutServerLoad;
