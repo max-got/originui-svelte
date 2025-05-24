f94349bf14ca8917817941abaefeee920811d1b1
Max G. <max.gottschalk@mailbox.org>
2025-05-24 18:42:13 +0200

    fix: update timeline item styles for improved layout and background color handling

--- src/lib/components/ui/timeline/timeline-item.svelte
+++ src/lib/components/ui/timeline/timeline-item.svelte
@@ -17,7 +17,8 @@
 <div
 	bind:this={ref}
 	class={cn(
-		'group/timeline-item relative flex flex-1 flex-col gap-0.5 group-data-[orientation=horizontal]/timeline:mt-8 group-data-[orientation=vertical]/timeline:ms-8 [&:has(+[data-completed])_[data-slot=timeline-separator]]:bg-primary group-data-[orientation=horizontal]/timeline:[&:not(:last-child)]:pe-8 group-data-[orientation=vertical]/timeline:[&:not(:last-child)]:pb-12',
+		'group/timeline-item relative flex flex-1 flex-col gap-0.5 group-data-[orientation=horizontal]/timeline:mt-8 group-data-[orientation=vertical]/timeline:ms-8 group-data-[orientation=horizontal]/timeline:[&:not(:last-child)]:pe-8 group-data-[orientation=vertical]/timeline:[&:not(:last-child)]:pb-12',
+		'[&:has(+[data-completed="true"])_[data-slot=timeline-separator]]:bg-primary',
 		className
 	)}
 	data-slot="timeline-item"
