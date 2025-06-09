51d4a0bc213312a37e65a7178fbe9b3c5444d71e
Max G. <max.gottschalk@mailbox.org>
2025-06-09 12:02:55 +0200

    feat: add TeamSwitcher component to navbars

--- src/lib/components/_extras/navbars/index.ts
+++ src/lib/components/_extras/navbars/index.ts
@@ -2,5 +2,6 @@ export { default as InfoMenu } from './info-menu.svelte';
 export { default as Logo } from './logo.svelte';
 export { default as NotificationMenu } from './notification-menu.svelte';
 export { default as SettingsMenu } from './settings-menu.svelte';
+export { default as TeamSwitcher } from './team-switcher.svelte';
 export { default as ThemeToggle } from './theme-toggle.svelte';
 export { default as UserMenu } from './user-menu.svelte';
