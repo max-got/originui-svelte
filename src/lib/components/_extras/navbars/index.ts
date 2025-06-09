8382e969b56932727e0f002639f0cf496f96a082
Max G. <max.gottschalk@mailbox.org>
2025-06-09 10:31:37 +0200

    feat: add ThemeToggle component to navbar

--- src/lib/components/_extras/navbars/index.ts
+++ src/lib/components/_extras/navbars/index.ts
@@ -1,4 +1,5 @@
 export { default as InfoMenu } from './info-menu.svelte';
 export { default as Logo } from './logo.svelte';
 export { default as NotificationMenu } from './notification-menu.svelte';
+export { default as ThemeToggle } from './theme-toggle.svelte';
 export { default as UserMenu } from './user-menu.svelte';
