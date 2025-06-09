8dea166c91b3d5804cbb0694941f4d4db0e36f54
Max G. <max.gottschalk@mailbox.org>
2025-06-09 11:55:04 +0200

    feat: add SettingsMenu component

--- src/lib/components/_extras/navbars/index.ts
+++ src/lib/components/_extras/navbars/index.ts
@@ -1,5 +1,6 @@
 export { default as InfoMenu } from './info-menu.svelte';
 export { default as Logo } from './logo.svelte';
 export { default as NotificationMenu } from './notification-menu.svelte';
+export { default as SettingsMenu } from './settings-menu.svelte';
 export { default as ThemeToggle } from './theme-toggle.svelte';
 export { default as UserMenu } from './user-menu.svelte';
