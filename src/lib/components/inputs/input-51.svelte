5fb3187b37260a2173526c6a8dc0babb2573f394
Max G. <max.gottschalk@mailbox.org>
2025-06-15 14:34:02 +0200

    refactor: improve input-51

--- src/lib/components/inputs/input-51.svelte
+++ src/lib/components/inputs/input-51.svelte
@@ -24,11 +24,10 @@
 				placeholder="Password"
 				type={passwordStrength.isVisible ? 'text' : 'password'}
 				bind:value={passwordStrength.password}
-				aria-invalid={passwordStrength.strengthScore < 4}
 				aria-describedby={passwordStrength.id}
 			/>
 			<button
-				class="text-muted-foreground/80 hover:text-foreground focus-visible:border-ring focus-visible:text-foreground focus-visible:ring-ring/30 absolute inset-y-px end-px flex h-full w-9 items-center justify-center rounded-e-lg transition-shadow focus-visible:border focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-hidden disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50"
+				class="text-muted-foreground/80 hover:text-foreground focus-visible:border-ring focus-visible:ring-ring/50 absolute inset-y-0 end-0 flex h-full w-9 items-center justify-center rounded-e-md transition-[color,box-shadow] outline-none focus:z-10 focus-visible:ring-[3px] disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50"
 				type="button"
 				onclick={passwordStrength.toggleVisibility}
 				aria-label={passwordStrength.isVisible ? 'Hide password' : 'Show password'}
